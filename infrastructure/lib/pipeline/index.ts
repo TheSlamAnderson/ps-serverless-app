8d632381d7de583b1b30784e1befb7aa31985656
Chris Anderson <Chris.Anderson@vynecorp.com>
2023-01-06 19:26:35 -0500

    Creating the Continuous Delivery Pipeline

---
+++ infrastructure/lib/pipeline/index.ts
@@ -0,0 +1,46 @@
+import * as cdk from '@aws-cdk/core';
+import { CodePipeline, ShellStep, CodePipelineSource } from '@aws-cdk/pipelines';
+import { LinuxBuildImage, BuildSpec } from '@aws-cdk/aws-codebuild';
+import { ApplicationStack } from '../core';
+
+class AppStage extends cdk.Stage {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StageProps) {
+    super(scope, id, props);
+    new ApplicationStack(this, 'AppStack');
+  }
+}
+
+export class ApplicationPipelineStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const source = CodePipelineSource.gitHub('TheSlamAnderson/ps-serverless-app', 'main', {
+      authentication: cdk.SecretValue.secretsManager('dms_config', {
+        jsonField: 'github_token',
+      }),
+    });
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      dockerEnabledForSynth: true,
+      codeBuildDefaults: {
+        buildEnvironment: {
+          buildImage: LinuxBuildImage.AMAZON_LINUX_2_3,
+        },
+        partialBuildSpec: BuildSpec.fromObject({
+          phases: {
+            install: {
+              commands: ['n 16.15.1'],
+            },
+          },
+        }),
+      },
+      synth: new ShellStep('Synth', {
+        input: source,
+        commands: ['yarn install --frozen-lockfile', 'cd infrastructure', 'npx cdk synth'],
+        primaryOutputDirectory: 'infrastructure/cdk.out',
+      }),
+    });
+
+    pipeline.addStage(new AppStage(this, 'Staging'));
+  }
+}
